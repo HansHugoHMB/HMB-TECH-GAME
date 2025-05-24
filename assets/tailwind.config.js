03d60daccbdc5492d595f8b5c70fb8696b1456eb
HansHugoHMB <mbayahans@gmail.com>
2025-05-24 01:27:47 +0100

    Update tailwind.config.js

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,1 +1,12 @@
-.
+// assets/tailwind.config.js
+module.exports = {
+  content: [
+    './js/**/*.js',
+    '../lib/*_web/**/*.*ex',
+    '../lib/*_web/**/*.heex'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
