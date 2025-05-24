d2c62db2815d96e98d09da2fb7d6fc838df67574
HansHugoHMB <mbayahans@gmail.com>
2025-05-24 01:29:05 +0100

    Create config/config.exs

---
+++ config/config.exs
@@ -0,0 +1,48 @@
+# config/config.exs
+import Config
+
+# Configure tailwind
+config :tailwind,
+  version: "3.3.2",
+  default: [
+    args: ~w(
+      --config=tailwind.config.js
+      --input=css/app.css
+      --output=../priv/static/assets/app.css
+    ),
+    cd: Path.expand("../assets", __DIR__)
+  ]
+
+# Configure esbuild
+config :esbuild,
+  version: "0.17.11",
+  default: [
+    args: ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+    cd: Path.expand("../assets", __DIR__),
+    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
+  ]
+
+# Configures the endpoint
+config :word_game, WordGameWeb.Endpoint,
+  url: [host: "localhost"],
+  render_errors: [view: WordGameWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: WordGame.PubSub,
+  live_view: [signing_salt: "YOUR_SIGNING_SALT"]
+
+# Configure esbuild (the version is required)
+config :esbuild,
+  version: "0.17.11",
+  default: [
+    args:
+      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+    cd: Path.expand("../assets", __DIR__),
+    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
+  ]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
